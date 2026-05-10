90efb1fa91dc42d2353eb8adde22ae749a049bc1
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2026-05-10 03:13:28 +0100

    Creation de l'interface SignalementRepository.

---
+++ tpiuc/src/main/java/com/iuc/tpiuc/repository/SignalementRepository.java
@@ -0,0 +1,14 @@
+package com.iuc.tpiuc.repository;
+
+import com.iuc.tpiuc.model.Signalement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+
+public interface SignalementRepository extends JpaRepository<Signalement, Long> {
+
+    List<Signalement> findByProfesseurId(Long professeurId);
+
+    List<Signalement> findByMaterielId(Long materielId);
+
+}
--- tpiuc/src/main/java/com/iuc/tpiuc/repository/UtilisateurRepository.java
+++ tpiuc/src/main/java/com/iuc/tpiuc/repository/UtilisateurRepository.java
@@ -3,5 +3,10 @@ package com.iuc.tpiuc.repository;
 import com.iuc.tpiuc.model.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
+
+    Optional<Utilisateur> findByEmail(String email);
+
 }
