d3365fb05ed82bc868a521915bbed912f6267fe0
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2026-05-10 03:24:38 +0100

    Creation de la classe UtilisateurResponseDTO.

---
+++ tpiuc/src/main/java/com/iuc/tpiuc/dto/response/UtilisateurResponseDTO.java
@@ -0,0 +1,22 @@
+package com.iuc.tpiuc.dto.response;
+
+
+
+import com.iuc.tpiuc.enums.Role;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UtilisateurResponseDTO {
+
+    private Long id;
+
+    private String nom;
+
+    private String prenom;
+
+    private String email;
+
+    private Role role;
+}
