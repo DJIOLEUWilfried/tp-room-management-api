53db68bf90bc16fb94001415403e52a68d0cfaf4
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2026-05-10 03:25:35 +0100

    Creation de la classe SalleRequestDTO.

---
+++ tpiuc/src/main/java/com/iuc/tpiuc/dto/request/SalleRequestDTO.java
@@ -0,0 +1,18 @@
+package com.iuc.tpiuc.dto.request;
+
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
+public class SalleRequestDTO {
+
+    @NotBlank(message = "Le nom de la salle est obligatoire")
+    private String nom;
+
+    @NotNull(message = "La capacité est obligatoire")
+    @Min(value = 1, message = "La capacité doit être supérieure à 0")
+    private Integer capacite;
+
+    private Boolean disponible;
+}
