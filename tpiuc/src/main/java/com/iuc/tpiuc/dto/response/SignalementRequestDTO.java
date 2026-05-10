981b2299b5573b2409033032b5d1c8283690f23e
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2026-05-10 03:37:06 +0100

    Création de la classe SignalementRequestDTO.

---
+++ tpiuc/src/main/java/com/iuc/tpiuc/dto/response/SignalementRequestDTO.java
@@ -0,0 +1,18 @@
+package com.iuc.tpiuc.dto.response;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class SignalementRequestDTO {
+
+    @NotBlank(message = "La description est obligatoire")
+    private String description;
+
+    @NotNull(message = "Le professeur est obligatoire")
+    private Long professeurId;
+
+    @NotNull(message = "Le matériel est obligatoire")
+    private Long materielId;
+}
