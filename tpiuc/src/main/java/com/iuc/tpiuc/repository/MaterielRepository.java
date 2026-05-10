9cf8efcde6d9aaeb91422a77808a02cff1aa72d2
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2026-05-10 03:08:35 +0100

    Creation de l'interface MaterielRepository.

---
+++ tpiuc/src/main/java/com/iuc/tpiuc/repository/MaterielRepository.java
@@ -0,0 +1,7 @@
+package com.iuc.tpiuc.repository;
+
+import com.iuc.tpiuc.model.Materiel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MaterielRepository extends JpaRepository<Materiel, Long> {
+}
