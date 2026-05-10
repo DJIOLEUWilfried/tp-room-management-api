709c4ef5459478282eb3c586dec7736d71f63ce7
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2026-05-10 03:31:34 +0100

    Création de la classe MaterielResponseDTO.

---
+++ tpiuc/src/main/java/com/iuc/tpiuc/dto/response/MaterielResponseDTO.java
@@ -0,0 +1,21 @@
+package com.iuc.tpiuc.dto.response;
+
+
+import com.iuc.tpiuc.enums.MaterielEtat;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MaterielResponseDTO {
+
+    private Long id;
+
+    private String nom;
+
+    private Integer quantite;
+
+    private MaterielEtat etat;
+
+    private String salle;
+}
