96961cc7518dbc3afc9ce39f456175e265c45b67
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2026-05-11 05:33:20 +0100

    Modification de la classe MaterielRepository.

--- tpiuc/src/main/java/com/iuc/tpiuc/repository/MaterielRepository.java
+++ tpiuc/src/main/java/com/iuc/tpiuc/repository/MaterielRepository.java
@@ -1,7 +1,13 @@
 package com.iuc.tpiuc.repository;
 
+import com.iuc.tpiuc.enums.MaterielEtat;
 import com.iuc.tpiuc.model.Materiel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MaterielRepository extends JpaRepository<Materiel, Long> {
+
+    List<Materiel> findByEtat(MaterielEtat etat);
+
 }
