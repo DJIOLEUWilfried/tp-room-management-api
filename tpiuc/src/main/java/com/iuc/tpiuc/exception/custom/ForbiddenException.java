c404a74a317774b950e69dfb21825d73f9dbd01f
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2026-05-11 15:33:57 +0100

    Création de l'exception personnaliseé ForbiddenException.

---
+++ tpiuc/src/main/java/com/iuc/tpiuc/exception/custom/ForbiddenException.java
@@ -0,0 +1,9 @@
+package com.iuc.tpiuc.exception.custom;
+
+public class ForbiddenException extends RuntimeException {
+
+    public ForbiddenException(String message) {
+        super(message);
+    }
+
+}
