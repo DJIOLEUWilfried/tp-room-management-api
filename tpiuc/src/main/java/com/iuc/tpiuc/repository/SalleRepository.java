e663e3a6e2e35d0c635ac35ff2e3e629505c31ab
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2026-05-11 05:32:33 +0100

    Modification de la classe SalleRepository.

--- tpiuc/src/main/java/com/iuc/tpiuc/repository/SalleRepository.java
+++ tpiuc/src/main/java/com/iuc/tpiuc/repository/SalleRepository.java
@@ -3,5 +3,9 @@ package com.iuc.tpiuc.repository;
 import com.iuc.tpiuc.model.Salle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SalleRepository extends JpaRepository<Salle, Long> {
+
+    List<Salle> findByDisponibleTrue();
 }
