9bd59dd89f4a032747bafad5042e4d85baa45e28
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2026-05-11 15:34:38 +0100

    Création de l'exception personnaliseé ResourceAlreadyExistsException.

---
+++ tpiuc/src/main/java/com/iuc/tpiuc/exception/custom/ResourceAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.iuc.tpiuc.exception.custom;
+
+public class ResourceAlreadyExistsException extends RuntimeException {
+
+    public ResourceAlreadyExistsException(String message) {
+        super(message);
+    }
+
+}
