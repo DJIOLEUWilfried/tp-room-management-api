743265d833d217d0fbfadcc72e6ec6b2258c78d3
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2026-05-11 05:34:07 +0100

    Modification de la classe ReservationRepository.

--- tpiuc/src/main/java/com/iuc/tpiuc/repository/ReservationRepository.java
+++ tpiuc/src/main/java/com/iuc/tpiuc/repository/ReservationRepository.java
@@ -1,7 +1,19 @@
 package com.iuc.tpiuc.repository;
 
+import com.iuc.tpiuc.enums.ReservationStatus;
 import com.iuc.tpiuc.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    List<Reservation> findByUtilisateurId(Long utilisateurId);
+
+    List<Reservation> findByStatus(ReservationStatus status);
+
+    List<Reservation> findByDateReservation(LocalDate date);
+
+
 }
