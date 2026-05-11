e0769e11124ec1fa2318752f493635d694790992
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2026-05-11 15:54:55 +0100

    Création de l'interface SalleService.

---
+++ tpiuc/src/main/java/com/iuc/tpiuc/service/SalleService.java
@@ -0,0 +1,19 @@
+package com.iuc.tpiuc.service;
+
+import com.iuc.tpiuc.dto.request.SalleRequestDTO;
+import com.iuc.tpiuc.dto.response.SalleResponseDTO;
+
+import java.util.List;
+
+public interface SalleService {
+
+    SalleResponseDTO create(SalleRequestDTO dto);
+
+    SalleResponseDTO update(Long id, SalleRequestDTO dto);
+
+    SalleResponseDTO getById(Long id);
+
+    List<SalleResponseDTO> getAll();
+
+    boolean delete(Long id);
+}
--- tpiuc/src/main/java/com/iuc/tpiuc/service/impl/UtilisateurServiceImpl.java
+++ tpiuc/src/main/java/com/iuc/tpiuc/service/impl/UtilisateurServiceImpl.java
@@ -2,11 +2,13 @@ package com.iuc.tpiuc.service.impl;
 
 import com.iuc.tpiuc.dto.request.UtilisateurRequestDTO;
 import com.iuc.tpiuc.dto.response.UtilisateurResponseDTO;
+import com.iuc.tpiuc.exception.custom.ResourceNotFoundException;
 import com.iuc.tpiuc.mapper.UtilisateurMapper;
 import com.iuc.tpiuc.model.Utilisateur;
 import com.iuc.tpiuc.repository.UtilisateurRepository;
 import com.iuc.tpiuc.service.UtilisateurService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +17,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 @Transactional
 public class UtilisateurServiceImpl implements UtilisateurService {
 
@@ -37,7 +40,7 @@ public class UtilisateurServiceImpl implements UtilisateurService {
 
         Utilisateur utilisateur = utilisateurRepository.findById(id)
                 .orElseThrow(() ->
-                        new RuntimeException("Utilisateur introuvable"));
+                        new ResourceNotFoundException(String.format("Utilisateur introuvable avec l'id: %d", id)));
 
         return UtilisateurMapper.toResponseDTO(utilisateur);
 
