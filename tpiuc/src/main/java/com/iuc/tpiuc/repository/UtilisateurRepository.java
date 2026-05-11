2269055ee08682089371314f06cbe7441915b6a1
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2026-05-11 05:31:27 +0100

    Modification de la classe UtilisateurRepository.

--- tpiuc/src/main/java/com/iuc/tpiuc/repository/UtilisateurRepository.java
+++ tpiuc/src/main/java/com/iuc/tpiuc/repository/UtilisateurRepository.java
@@ -1,12 +1,18 @@
 package com.iuc.tpiuc.repository;
 
+import com.iuc.tpiuc.enums.Role;
 import com.iuc.tpiuc.model.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
 
     Optional<Utilisateur> findByEmail(String email);
+
+    Optional<Utilisateur> findByCode(String code);
+
+    List<Utilisateur> findByRole(Role role);
 
 }
