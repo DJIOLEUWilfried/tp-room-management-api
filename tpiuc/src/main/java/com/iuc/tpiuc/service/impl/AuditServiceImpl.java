cf59e07ac902c24436d571d1156bd7de0852f406
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2026-05-15 06:57:20 +0100

    Création de la classe AuditServiceImpl.

---
+++ tpiuc/src/main/java/com/iuc/tpiuc/service/impl/AuditServiceImpl.java
@@ -0,0 +1,13 @@
+package com.iuc.tpiuc.service.impl;
+
+import com.iuc.tpiuc.model.Utilisateur;
+import com.iuc.tpiuc.service.AuditService;
+
+public class AuditServiceImpl  implements AuditService {
+
+
+    @Override
+    public void save(String action, Utilisateur utilisateur) {
+
+    }
+}
