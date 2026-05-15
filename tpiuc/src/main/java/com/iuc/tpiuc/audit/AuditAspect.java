db979609bcb4df51702d642713f23e3795b2ad49
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2026-05-15 07:08:23 +0100

    Création  de la methode saveAudit() de la classe AuditAspect.

--- tpiuc/src/main/java/com/iuc/tpiuc/audit/AuditAspect.java
+++ tpiuc/src/main/java/com/iuc/tpiuc/audit/AuditAspect.java
@@ -1,8 +1,12 @@
 package com.iuc.tpiuc.audit;
 
 
+import com.iuc.tpiuc.model.Utilisateur;
+import com.iuc.tpiuc.service.AuditService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
@@ -11,4 +15,52 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Slf4j
 public class AuditAspect {
+
+    private final AuditService auditService;
+
+    @AfterReturning(
+            value = "@annotation(auditTrace)",
+            returning = "result"
+    )
+    public void saveAudit(
+            JoinPoint joinPoint,
+            AuditTrace auditTrace,
+            Object result
+    ) {
+
+        log.info("\n Déclenchement audit automatique");
+
+        try {
+
+            // RECUPERATION ACTION
+            String action = auditTrace.action();
+
+            // RECUPERATION UTILISATEUR CONNECTE
+            Utilisateur utilisateur = getCurrentUser();
+
+            // SAVE AUDIT
+            auditService.save( action, utilisateur );
+
+            log.info("\n Audit automatique effectué");
+
+        } catch (Exception e) {
+
+            log.error(
+                    "Erreur aspect audit",
+                    e
+            );
+        }
+    }
+
+    // ==========================================
+    // TEMPORAIRE
+    // ==========================================
+
+    private Utilisateur getCurrentUser() {
+
+        // Plus tard :
+        // Spring Security JWT
+
+        return null;
+    }
 }
