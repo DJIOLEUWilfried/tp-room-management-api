b53ca8f404a6d8b3093cf26fc4cf8516df0f63b5
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2026-05-15 06:41:15 +0100

    Création de l'interface AuditTrace.

---
+++ tpiuc/src/main/java/com/iuc/tpiuc/audit/AuditTrace.java
@@ -0,0 +1,12 @@
+package com.iuc.tpiuc.audit;
+
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface AuditTrace {
+
+    String action();
+}
