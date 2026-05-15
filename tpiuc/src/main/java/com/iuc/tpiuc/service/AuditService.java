1297c354e71f679e1979485bca3f01c50ff20e9e
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2026-05-15 07:41:22 +0100

    Modification de l'interface AuditService.

--- tpiuc/src/main/java/com/iuc/tpiuc/service/AuditService.java
+++ tpiuc/src/main/java/com/iuc/tpiuc/service/AuditService.java
@@ -1,8 +1,15 @@
 package com.iuc.tpiuc.service;
 
+import com.iuc.tpiuc.dto.response.AuditResponseDTO;
 import com.iuc.tpiuc.model.Utilisateur;
+
+import java.util.List;
 
 public interface AuditService {
 
     void save( String action, Utilisateur utilisateur );
+
+    List<AuditResponseDTO> getAll();
+
+    List<AuditResponseDTO> getByUtilisateur(Long Utilisateur);
 }
